deb43a994e7bc4bdde8d927537bdd8c055b9f354
Franklin Ezeyim <ezeyimf@gmail.com>
2026-02-10 13:20:50 +0100

    jwt fix

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -35,8 +35,8 @@ export const registerUser = async(req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
-            maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax', // ✅ FIXED
+            maxAge: 7 * 24 * 60 * 60 * 1000
         })
     
         const {hashedPassword, ...detailsWithoutPassword} = user.toObject()
